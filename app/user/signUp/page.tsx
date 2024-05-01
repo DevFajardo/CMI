a56f4dcdd0e7fe6d0a2f745cad9aea8c21e3d3da
AlejoCant <alejandrocantillop.ilusa@gmail.com>
2024-05-01 18:49:47 -0500

    Some issues 'fixed' sign in button and sign Up

--- app/user/signUp/page.tsx
+++ app/user/signUp/page.tsx
@@ -16,19 +16,19 @@ export default function userSignUp() {
             [e.target.name]: e.target.value
         });
     }
-    const areFieldsNotEmpty = fields.name !== "" && fields.lastName !== "" && fields.email !== "" && fields.password !== "" && fields.repeatpassword !== "";
+    const areFieldsNotEmpty = fields.name !== "" && fields.lastName !== "" && fields.email !== "" && fields.password !== "" && fields.repeatpassword !== "" && fields.password === fields.repeatpassword;
     return (
         <div>
             <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-                <label htmlFor="name" className="text-md">Nombre</label>
-                <input type="text" name="name" placeholder="nombre completo" value={fields.name} onChange={handleFieldChange} />
+                <label htmlFor="name" className="text-md">Nombres</label>
+                <input type="text" name="name" placeholder="nombres" value={fields.name} onChange={handleFieldChange} />
                 <label htmlFor="lastName" className="text-md">Apellidos</label>
                 <input type="text" name="lastName" placeholder="Apellidos" value={fields.lastName} onChange={handleFieldChange} />
                 <label htmlFor="email" className="text-md">Correo</label>
                 <input type="email" name="email" placeholder="nombreusuario@email.com" value={fields.email} onChange={handleFieldChange} />
                 <label htmlFor="password" className="text-md">Contraseña</label>
                 <input type="password" name="password" placeholder="••••••••" value={fields.password} onChange={handleFieldChange} />
-                <label htmlFor="repeatpassword">repita su contraseña</label>
+                <label htmlFor="repeatpassword">Repetir contraseña</label>
                 <input type="password" name="repeatpassword" placeholder="••••••••" value={fields.repeatpassword} onChange={handleFieldChange} />
                 <SubmitButton
                     formAction={signUp}
@@ -36,7 +36,7 @@ export default function userSignUp() {
                     pendingText="Signing Up..."
                     disabled={!areFieldsNotEmpty}
                 >
-                    Sign Up
+                    Registrarse
                 </SubmitButton>
             </form>
         </div>
